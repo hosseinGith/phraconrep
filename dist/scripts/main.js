4aad2270c63b7d71e55c01f5b845b38a3d6dd4e7
hosseinGith <hosseinderees7@gmail.com>
2025-07-17 12:51:41 +0330

    swiper

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,1 +1,23 @@
 "use strict";
+new Swiper(".firstSliderHomePage", {
+  direction: "horizontal",
+  loop: false,
+  slidesPerView: 'auto',
+  spaceBetween: 10,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      width: null,
+    },
+    500: {
+      slidesPerView: 'auto',
+      spaceBetween: 10,
+      width: null,
+    },
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
